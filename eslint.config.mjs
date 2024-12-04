83260ec24e13ada8f826cce3b840d6e6869005a2
CyberGlitch <komavrellis@gmail.com>
2024-12-04 23:48:44 +0200

    Project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import unusedImports from "eslint-plugin-unused-imports";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import eslintPluginDrizzle from "eslint-plugin-drizzle"
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+    plugins: {
+        "drizzle": eslintPluginDrizzle,
+        "unused-imports": unusedImports,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+
+        'drizzle/enforce-delete-with-where': 'error',
+        'drizzle/enforce-update-with-where': 'error',
+
+        "unused-imports/no-unused-vars": ["warn", {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+        }],
+    },
+});
